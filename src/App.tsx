8faf1c863e6f3c191307f90885c12cc6d65643bc
lvukad01 <lana.vukadin1234@gmail.com>
2026-03-03 14:15:01 +0100

    Initialize

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/launches">Launches</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home - SpaceX Info</h1>} />
+        <Route path="/launches" element={<h1>Launches Page</h1>} />
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
